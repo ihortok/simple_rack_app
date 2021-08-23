1bd63c84d5050ae67eed4ce4a0dd720788fdd6b1
ihortok <itokaryk@gmail.com>
2021-08-23 23:48:34 +0300

    NotFoundError

--- app.rb
+++ app.rb
@@ -1,8 +1,44 @@
 # frozen_string_literal: true
 
+$routes = ['/', '/contacts', '/about']
+
+# NotFoundError Rack middleware
+class NotFoundError
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    if $routes.include? env['PATH_INFO']
+      @app.call(env)
+    else
+      [404, { 'Contend-Type' => 'text/plain' }, ['Page Not Found']]
+    end
+  end
+end
+
 # Simple Rack App
 class App
   def call(env)
-    [200, { 'Contend-Type' => 'text/plain' }, ['Hello!']]
+    case env['PATH_INFO']
+    when '/'
+      [
+        200,
+        { 'Contend-Type' => 'text/plain' },
+        ['Hey there!', 'It is a home page!']
+      ]
+    when '/contacts'
+      [
+        200,
+        { 'Contend-Type' => 'text/plain' },
+        ['Hey there!', 'It is a contacts page!']
+      ]
+    when '/about'
+      [
+        200,
+        { 'Contend-Type' => 'text/plain' },
+        ['Hey there!', 'It is a about page!']
+      ]
+    end
   end
 end
